d2a67e61abb95e19073b3a3d75524f9de8762df4
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-06-20 11:01:08 +0700

    debug sale process2

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -59,7 +59,7 @@ const sale = async (req, res) => {
     if (listOfOverNumber.length < 1) {
         logger.error("Total transaction: " + item.length);
         const bill_num = await get_billnum();
-        const sqlCommand = formSqlCommandForMultiRows(item, bill_num,ism)
+        const sqlCommand = formSqlCommandForMultiRows(item, bill_num,ism,user,qr_code)
         console.log("SQL: " + sql);
 
         conn.query(sqlCommand,
@@ -81,7 +81,7 @@ const sale = async (req, res) => {
         res.send(listOfOverNumber);
     }
 };
-const formSqlCommandForMultiRows = (sale, bill_num,ism) => {
+const formSqlCommandForMultiRows = (sale, bill_num,ism,user,qr_code) => {
     const sqlValueConcatination = '';
     for (let i = 0; i < sale.length; i++) {
         const colon = i < sale.length - 1 ? "," : ";";
